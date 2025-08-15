6a8c9fe84211e24661052e00ebb06663235f9774
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-08-15 13:51:10 +0530

    Backend and other changes

---
+++ mymovies/backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+//Files
+import connectDB from "./config/db.js";
+//configuration
+dotenv.config(); //to parse .env into process.env
+connectDB();
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.POST || 3000;
+
+//Routes
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
