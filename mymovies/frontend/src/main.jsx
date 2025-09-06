978fb0cabc1b508d5f516d7636ea57f4abedd8ad
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-09-06 08:55:47 +0530

    Frontend and the redux store

--- mymovies/frontend/src/main.jsx
+++ mymovies/frontend/src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- mymovies/frontend/src/redux/store.js
+++ mymovies/frontend/src/redux/store.js
@@ -1,2 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListener } from "@reduxjs/toolkit/query/react";
+const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apliSlice.middleware),
+  devTools: true,
+});
